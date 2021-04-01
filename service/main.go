d326acc14cab0691f2ad1fa726efd072049740f1
Sergey <sssergei@hotmail.com>
2021-04-01 16:48:05 +0300

    new update

--- service/main.go
+++ service/main.go
@@ -11,7 +11,7 @@ import (
 	"net"
 	"time"
 
-	pb "github.com/sssergei/BaseService/tree/main/proto"
+	pb "github.com/sssergei/BaseService/proto"
 	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
