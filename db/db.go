e4fdfae881dce4b54a14b19bd4ce9e901a60329f
Sergey <sssergei@hotmail.com>
2021-04-05 16:43:47 +0300

    changes to other directory

---
+++ db/db.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func InsertUser(id int, name string, surname string, othername string) {
+	fmt.Println("Go MySQL database")
+	db, err := sql.Open("mysql", "root:QWEfghIOP0!@tcp(127.0.0.1:3306)/mydb")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+	insert, err := db.Query("INSERT INTO users VALUES ( 1, 'TEST' , 'Test1','Test2' )")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Go MySQL database %d ", insert.Scan("id"))
+	defer insert.Close()
+}
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err := sql.Open("mysql", "root:QWEfghIOP0!@tcp(127.0.0.1:3306)/mydb")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+
+	// perform a db.Query insert
+	insert, err := db.Query("INSERT INTO users VALUES ( 1, 'TEST' , 'Test1','Test2' )")
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
+
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,9 @@ module github.com/sssergei/BaseService
 
 go 1.16
 
-replace github.com/sssergei/BaseService => ../service
-replace github.com/sssergei/BaseService/db => ../db
-
 require (
+	github.com/sssergei/BaseService/db v0.0.0-20210405114840-2bccdf1b4909
 	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c
 	google.golang.org/grpc v1.36.1
-	google.golang.org/protobuf v1.25.0 // indirect
+	google.golang.org/protobuf v1.25.0
 )
